d99a374dc1e30ae21629e440be50413291011d6a
Anton Maximov <53797863+AntonMAXI@users.noreply.github.com>
2019-08-29 23:48:33 +0300

    Update hw04_normal.py

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,6 @@
+
+import re
+import random
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -19,12 +22,14 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
-
+reg_one = r'[a-z]+'
+res = re.findall(reg_one, line)
+print(res)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
-# Т.е. из строки 
+# Т.е. из строки
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
@@ -45,9 +50,74 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+reg_one = r'(?<=[a-z]{2})[A-Z]+(?=[A-Z]{2})'
+res = re.findall(reg_one, line)
+print(res)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+text_write = open('text.txt', 'w')
+
+with open('text.txt', 'w') as i:
+       for a in range(2500-1):
+              text_write.write(str(random.randint(0,9)))
+       text_write.close()
+
+text_read = open('text.txt', 'r')
+i = text_read.readline(2500)
+print(i)
+a = list(i)
+a.sort()
+
+l_0 = []
+l_1 = []
+l_2 = []
+l_3 = []
+l_4 = []
+l_5 = []
+l_6 = []
+l_7 = []
+l_8 = []
+l_9 = []
+for f in range(1, len(i)-1):
+       if i[f] == '0':
+               #== len(i[f+1]):
+              l_0.append(i[f])
+       elif i[f] == '1':
+              l_1.append(i[f])
+
+       elif i[f] == '2':
+              l_2.append(i[f])
+
+       elif i[f] == '3':
+              l_3.append(i[f])
+
+       elif i[f] == '4':
+              l_4.append(i[f])
+
+       elif i[f] == '5':
+              l_5.append(i[f])
+
+       elif i[f] == '6':
+              l_6.append(i[f])
+
+       elif i[f] == '7':
+              l_7.append(i[f])
+
+       elif i[f] == '8':
+              l_8.append(i[f])
+
+       elif i[f] == '9':
+              l_9.append(i[f])
+
+lst_new = [[l_0], [l_1], [l_2], [l_3], [l_4], [l_5], [l_6], [l_7], [l_8], [l_9]]
+# func = lambda el1, el2: '%s|%s' % (el1, el2)
+for elem in lst_new:
+       print(elem)
+text_read.close()
+# Увидел каких цифр больше в списке, но не дошел до вывода наиболее длинного элемента списка списков
