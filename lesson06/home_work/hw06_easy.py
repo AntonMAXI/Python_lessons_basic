4f81fa85256b87ec89bd7cda12af7f867ddf62d1
Anton Maximov <53797863+AntonMAXI@users.noreply.github.com>
2019-09-10 01:14:37 +0300

    New Update less_06

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -10,48 +10,49 @@ class Treug():
         self.A = A
         self.B = B
         self.C = C
-        self.st_AB = math.sqrt((self.B[0] - self.A[0]) ** 2 + (self.B[1] - self.A[1]) ** 2)
-        self.st_AC = math.sqrt((self.C[0] - self.A[0]) ** 2 + (self.C[1] - self.A[1]) ** 2)
-        self.st_BC = math.sqrt((self.C[0] - self.B[0]) ** 2 + (self.C[1] - self.B[1]) ** 2)
+
         print('Точки треуголника заданы')
 
-    if __name__ == '__main__':
-        @property
-        def vis(self):
-            visot = (self.st_AB + self.st_AC + self.st_BC) * 1 / 2
-            return float(visot)
+    def dlina_AB(self):
+        self.st_AB = math.sqrt((self.B[0] - self.A[0]) ** 2 + (self.B[1] - self.A[1]) ** 2)
+        print('Длина стороны АВ равна :')
+        return self.st_AB
 
-    if __name__ == '__main__':
-        @property
-        def per(self):
-            perimetr = self.st_AB + self.st_AC + self.st_BC
-            return float(perimetr)
+    def dlina_AC(self):
+        self.st_AC = math.sqrt((self.C[0] - self.A[0]) ** 2 + (self.C[1] - self.A[1]) ** 2)
+        print('Длина стороны АС равна :')
+        return self.st_AC
 
-    if __name__ == '__main__':
-        @property
-        def plo(self):
-            plosh = 1 / 2 * (self.st_AB * ((self.st_AB + self.st_AC + self.st_BC) * 1 / 2))
-            return float(plosh)
+    def dlina_BC(self):
+        self.st_BC = math.sqrt((self.C[0] - self.B[0]) ** 2 + (self.C[1] - self.B[1]) ** 2)
+        print('Длина стороны ВС равна :')
+        return self.st_BC
 
+    def vis(self):
+        visot = (self.st_AB + self.st_AC + self.st_BC) * 1 / 2
+        print('Высота равна :')
+        return float(visot)
+
+    def per(self):
+        perimetr = self.st_AB + self.st_AC + self.st_BC
+        print('Периметр равен :')
+        return float(perimetr)
+
+    def plo(self):
+        plosh = 1 / 2 * (self.st_AB * ((self.st_AB + self.st_AC + self.st_BC) * 1 / 2))
+        print('Площадь равна :')
+        return float(plosh)
 
 Tr = Treug((2, 4), (4, 8), (6, 1))
 print(Tr.A)
 print(Tr.B)
 print(Tr.C)
-print('Длина стороны АВ равна :')
-print(Tr.st_AB)
-print('Длина стороны АС равна :')
-print(Tr.st_AC)
-print('Длина стороны ВС равна :')
-print(Tr.st_BC)
-print('Высота равна :')
-print(Tr.vis)
-print('Периметр равен :')
-print(Tr.per)
-print('Площадь равна :')
-print(Tr.plo)
-
-
+print(Tr.dlina_AB())
+print(Tr.dlina_AC())
+print(Tr.dlina_BC())
+print(Tr.vis())
+print(Tr.per())
+print(Tr.plo())
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
@@ -60,24 +61,32 @@ print(Tr.plo)
 
 class Ravtrap():
 
-    def __init__(self, A, B, C, D):
+    def __init__(self, A, B, C, D, do_AB=None, do_CD=None, do_AD=None):
         self.A = A
         self.B = B
         self.C = C
         self.D = D
+        self.do_AB = do_AB
+        self.do_CD = do_CD
+        self.do_AD = do_AD
+
+    @property
+    def checking(self):
         self.do_AB = math.sqrt((self.B[0] - self.A[0]) ** 2 + (self.B[1] - self.A[1]) ** 2)
         self.do_CD = math.sqrt((self.D[0] - self.C[0]) ** 2 + (self.D[1] - self.C[1]) ** 2)
         self.do_BC = math.sqrt((self.C[0] - self.B[0]) ** 2 + (self.C[1] - self.B[1]) ** 2)
         self.do_AD = math.sqrt((self.D[0] - self.A[0]) ** 2 + (self.D[1] - self.A[1]) ** 2)
-        self.H = math.sqrt(self.do_AB ** 2 -(((self.do_BC - self.do_AD) ** 2 + self.do_AB ** 2 - self.do_CD ** 2) / 2 * (self.do_BC - self.do_AD)) ** 2)
-
-    @property
-    def checking(self):
         if self.do_AB == self.do_CD or self.do_BC == self.do_AD:
-            s = print('Трапеция равнобедренная')
+            self.s = f'{"Трапеция равнобедренная"}'
         else:
-            s = print('Трапеция не равнобедренная')
-        return s
+            self.s = f'{"Трапеция не равнобедренная"}'
+        return self.s
+
+    def H(self):
+        self.H = math.sqrt(self.do_AB ** 2 - (
+                ((self.do_BC - self.do_AD) ** 2 + self.do_AB ** 2 - self.do_CD ** 2) / 2 * (
+                self.do_BC - self.do_AD)) ** 2)
+        return float(self.H)
 
     def AB(self):
         AB = math.sqrt((self.B[0] - self.A[0]) ** 2 + (self.B[1] - self.A[1]) ** 2)
@@ -103,14 +112,13 @@ class Ravtrap():
         plo = 4 * ((self.do_BC + self.do_AD) / 2)
         return plo
 
+
 rt = Ravtrap((0, 2), (0, 7), (2, 5), (2, 4))
 print('Проверка трапеции : ')
 print(rt.checking)
 print('Периметр трапеции равен :')
 print(rt.perim())
 print('Высота трапеции равна :')
-print(rt.H)
+print(rt.H())
 print('Площадь трапеции равна :')
 print(rt.plosh())
-
-
